fee3e3cb937060c0abb2ae1da1e46f74cd80da04
Diego <dierios93@gmail.com>
2025-05-09 20:30:10 -0500

    chore(landing): added landing to page

---
+++ src/sections/Landing.tsx
@@ -0,0 +1,15 @@
+import Benefits from '@/components/landing/Benefits'
+import Faq from '@/components/landing/Faq'
+import Hero from '@/components/landing/Hero'
+import Process from '@/components/landing/Process'
+
+export default function Landing() {
+  return (
+    <>
+      <Hero />
+      <Process />
+      <Benefits />
+      <Faq />
+    </>
+  )
+}
