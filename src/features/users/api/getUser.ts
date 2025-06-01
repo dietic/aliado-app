d4f1bb01684580e8fedde4198c25be4172a712d1
Diego <dierios93@gmail.com>
2025-06-01 13:32:53 -0500

    refactor(users/api): remove debug log from getUserByPhone

--- src/features/users/api/getUser.ts
+++ src/features/users/api/getUser.ts
@@ -1,6 +1,5 @@
 export async function getUserByPhone(params: { phone: string }) {
   try {
-    console.log('trying to call');
     const res = await fetch(`/api/users/phone/${params.phone}`);
     const body = await res.json();
     return body;
